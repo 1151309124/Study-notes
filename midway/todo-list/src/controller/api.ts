9062365d561677f494faf3fa965be69706ce8a17
1151309124 <115130924@qq.com>
2022-05-10 13:58:58 +0800

    fail

--- midway/todo-list/src/controller/api.ts
+++ midway/todo-list/src/controller/api.ts
@@ -2,7 +2,7 @@
  * @Author: 1151309124 115130924@qq.com
  * @Date: 2022-05-06 19:52:36
  * @LastEditors: 1151309124 115130924@qq.com
- * @LastEditTime: 2022-05-09 13:37:49
+ * @LastEditTime: 2022-05-10 13:56:55
  * @FilePath: \leetcodee:\vs CODE\midway\todo-list\src\controller\api.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,6 +40,11 @@ export class APIController {
   // GET /api/todo
   @Get('/todo')
   async getTodo() {
+    return todoList
+  }
+  
+  @Get('/todo')
+  async deleteTodo() {
     return todoList
   }
 }
